0e75108f57744582b3d8b9e46a196b52a53d999d
muflikhandimasd <muflikhan.dimas@gmail.com>
2022-04-23 21:11:59 +0700

    Initial commit

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,26 @@
+fun main() {
+//    var age : Byte = 22
+//    var height : Short = 22222
+//    var balance : Int = 23123231
+//    var distance : Long = 7373873283232L
+//    println(age)
+//    println(height)
+
+//    Floating Point Number
+    var float : Float = 99.222F
+    var double : Double = 2882828.2878278
+
+//    Literals
+    var binary : Int = 0b00100101010
+    var hexadecimalLiteral : Int = 0xff5363
+    var normalLiteral : Int = 121
+    println(binary)
+
+//    Underscore
+    var price: Int = 9_000_000 //Hanya untuk penanda
+    println(price)
+
+//    Conversion
+    var binaryToDouble : Double = binary.toDouble()
+    println(binaryToDouble)
+}
